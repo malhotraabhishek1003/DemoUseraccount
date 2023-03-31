9deff703f8d0f594e5152f3a1ef7c9ca364b7ca3
SS893YB <SS893YB@IN3272299W1.ey.net>
2023-03-31 16:24:23 +0530

    Second commit with mapping correction

--- NewDemoProject/src/main/java/com/demo/repo/UserAccountRepo.java
+++ NewDemoProject/src/main/java/com/demo/repo/UserAccountRepo.java
@@ -1,9 +1,11 @@
 package com.demo.repo;
 
 import java.util.Optional;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
 import com.demo.entity.UserAccount;
 
 @Repository
