fc0db0225f2f84667a1e9e791b611ab883cb9d53
abhishekmalhotra0312 <133968314+abhishekmalhotra0312@users.noreply.github.com>
2023-07-13 14:38:08 +0530

    Update UserAccountServiceImpl.java

--- NewDemoProject/src/main/java/com/demo/service/UserAccountServiceImpl.java
+++ NewDemoProject/src/main/java/com/demo/service/UserAccountServiceImpl.java
@@ -1,10 +1,12 @@
 package com.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -18,11 +20,9 @@ public class UserAccountServiceImpl implements UserAccountService {
 
 	@Autowired
 	UserAccountRepo accountRepo;
-	
+
 	@Autowired
 	InterestRepo interestRepo;
-	
-	
 
 	@Override
 	public UserAccount saveNewUserAccount(UserAccount account) {
@@ -33,7 +33,6 @@ public class UserAccountServiceImpl implements UserAccountService {
 		}
 	}
 
-	
 	@Override
 	public List<UserAccount> getUsers() {
 		// TODO Auto-generated method stub
@@ -41,14 +40,42 @@ public class UserAccountServiceImpl implements UserAccountService {
 
 	}
 
-
 	@Override
 	public Interest createInterest(Interest interest) {
 		// TODO Auto-generated method stub
-		
+
 		UserAccount userAccount = accountRepo.findById(interest.getUserAccount().getId()).orElse(null);
- 		interest.setUserAccount(userAccount);
- 		return interestRepo.save(interest);
+		interest.setUserAccount(userAccount);
+		return interestRepo.save(interest);
+	}
+
+	public Optional<UserAccount> getUsersById(int id) {
+
+		Optional<UserAccount> list = accountRepo.findById(id);
+		return list;
+	}
+
+	@Override
+	public ResponseEntity<UserAccount> updateUserAccount(int id) {
+
+		UserAccount employee = accountRepo.findById(id).orElse(null);
+
+		employee.setAge(21);
+		employee.setCity("Delhi");
+		// Update other properties as needed
+
+		UserAccount savedEmployee = accountRepo.save(employee);
+		return ResponseEntity.ok(savedEmployee);
+
+	}
+
+	@Override
+	public ResponseEntity<String> deleteUserAccount(int id) {
+		UserAccount employee = accountRepo.findById(id).orElse(null);
+
+		accountRepo.delete(employee);
+
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Employee deleted successfully");
 	}
 
 }
